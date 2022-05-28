e24a13b6496d72f55722d4937c22f63aeef2903c
Antti Pitkänen <anttis.pitkanen@gmail.com>
2022-05-28 15:34:07 +0300

    Rust: CLI args and SIGINT handling

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,12 @@
+use clap::{Arg, Command};
 use rand::Rng;
-use std::{thread, time};
+use std::{
+    sync::{self, atomic::AtomicBool, Arc},
+    thread, time,
+};
 mod logic;
 
-fn get_random_cell() -> bool {
+fn random_cell_value() -> bool {
     let mut rng = rand::thread_rng();
     return rng.gen_range(0..2) == 1;
 }
@@ -12,7 +16,7 @@ fn create_random_grid(side_length: i32) -> logic::Grid {
     for _ in 0..side_length {
         let mut row = Vec::new();
         for _ in 0..side_length {
-            row.push(get_random_cell());
+            row.push(random_cell_value());
         }
         grid.push(row);
     }
@@ -46,13 +50,72 @@ fn clear_screen() {
     print!("{esc}c", esc = 27 as char);
 }
 
-fn main() {
-    clear_screen();
-    let mut grid = create_random_grid(20);
+struct Args {
+    side_length: i32,
+    wait_time: f32,
+}
 
-    loop {
+fn parse_args() -> Args {
+    let matches = Command::new("Terminal Game of Life – Rust")
+        .version("1.0")
+        .author("Antti Pitkänen")
+        .about("A Rust implementation of the Game of Life in terminal")
+        .arg(
+            Arg::new("side_length")
+                .short('s')
+                .long("side_length")
+                .value_name("SIDE_LENGTH")
+                .help("The side length of the square grid")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::new("wait_time")
+                .short('w')
+                .long("wait_time")
+                .value_name("WAIT_TIME")
+                .help("Wait time between steps in seconds")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let side_length = matches
+        .value_of("side_length")
+        .unwrap_or("20")
+        .parse::<i32>()
+        .unwrap();
+    let wait_time = matches
+        .value_of("wait_time")
+        .unwrap_or("0.4")
+        .parse::<f32>()
+        .unwrap();
+
+    return Args {
+        side_length,
+        wait_time,
+    };
+}
+
+fn main() {
+    // Control loop, could this be extracted into a function?
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+
+    ctrlc::set_handler(move || {
+        r.store(false, sync::atomic::Ordering::SeqCst);
+    })
+    .expect("Error setting SIGINT handler");
+
+    let args = parse_args();
+    clear_screen();
+    let mut grid = create_random_grid(args.side_length);
+
+    // Could this be done the other way around? I.e. "if false then break"
+    while running.load(sync::atomic::Ordering::SeqCst) {
         print_grid(&grid);
         grid = logic::game_of_life_step(&grid);
-        thread::sleep(time::Duration::from_millis(400));
+        thread::sleep(time::Duration::from_secs_f32(args.wait_time));
     }
+
+    clear_screen();
+    println!("Exiting...");
 }
