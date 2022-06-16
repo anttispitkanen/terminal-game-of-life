77c6e8c0fb8528cb5bc52b2ea3c88fbcab211075
Antti Pitkänen <anttis.pitkanen@gmail.com>
2022-06-17 00:37:37 +0300

    Rust: validate max side length

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,6 +4,7 @@ use std::{
     sync::{self, atomic::AtomicBool, Arc},
     thread, time,
 };
+use termion;
 mod logic;
 mod rendering;
 
@@ -29,7 +30,7 @@ struct Args {
     wait_time: f32,
 }
 
-fn parse_args() -> Args {
+fn parse_args(max_side_length: i32) -> Args {
     let matches = Command::new("Terminal Game of Life – Rust")
         .version("1.0")
         .author("Antti Pitkänen")
@@ -39,7 +40,23 @@ fn parse_args() -> Args {
                 .short('s')
                 .long("side_length")
                 .value_name("SIDE_LENGTH")
-                .help("The side length of the square grid")
+                .help("The side length of the square grid (int)")
+                .validator(|f| {
+                    let side_length = match f.parse::<i32>() {
+                        Ok(side_length) => side_length,
+                        Err(_) => {
+                            return Err(format!("Invalid side length, must be integer"));
+                        }
+                    };
+                    if side_length > 2 && side_length <= max_side_length {
+                        Ok(())
+                    } else {
+                        Err(format!(
+                            "Invalid side length, must be between 3 and {}",
+                            max_side_length
+                        ))
+                    }
+                })
                 .takes_value(true),
         )
         .arg(
@@ -47,7 +64,7 @@ fn parse_args() -> Args {
                 .short('w')
                 .long("wait_time")
                 .value_name("WAIT_TIME")
-                .help("Wait time between steps in seconds")
+                .help("Wait time between steps in seconds (float)")
                 .takes_value(true),
         )
         .get_matches();
@@ -69,6 +86,19 @@ fn parse_args() -> Args {
     };
 }
 
+fn get_max_side_length(terminal_width: u16, terminal_height: u16) -> i32 {
+    // Terminal height as rows. Due to how the grid is printed, we can have max
+    // N-1 rows.
+    let max_height = terminal_height as i32 - 1;
+
+    // Terminal width as columns, 1 column = 1 character. Since we render "🟪 ",
+    // each game cell takes 3 characters. Thus max width is N/3.
+    let max_width: i32 = terminal_width as i32 / 3;
+
+    // Return the smaller of the two.
+    return std::cmp::min(max_height, max_width);
+}
+
 fn main() {
     // Control loop, could this be extracted into a function?
     let running = Arc::new(AtomicBool::new(true));
@@ -79,7 +109,10 @@ fn main() {
     })
     .expect("Error setting SIGINT handler");
 
-    let args = parse_args();
+    let (terminal_width, terminal_height) =
+        termion::terminal_size().expect("Unable to get terminal size");
+
+    let args = parse_args(get_max_side_length(terminal_width, terminal_height));
     rendering::clear_screen();
     let mut old_grid = create_random_grid(args.side_length);
     let mut new_grid = old_grid.clone();
