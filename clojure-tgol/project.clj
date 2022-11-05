ecb029c5f75b91326328cafffd0d60dd610c9cf1
Antti Pitkänen <anttis.pitkanen@gmail.com>
2022-11-05 17:54:16 +0200

    Clojure: logic module and tests

---
+++ clojure-tgol/project.clj
@@ -0,0 +1,5 @@
+(defproject clojure-tgol "0.1.0-SNAPSHOT"
+  :description "Comman line game of life with Clojure"
+  :url "https://github.com/anttispitkanen/terminal-game-of-life"
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :repl-options {:init-ns clojure-tgol.core})
